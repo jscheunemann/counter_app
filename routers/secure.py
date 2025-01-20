0629272153fb087735ac21afacdddbde7fb03227
Jason Scheunemann <jason.scheunemann@gmail.com>
2025-01-20 06:31:31 +0000

    Added test json files

--- routers/secure.py
+++ routers/secure.py
@@ -8,19 +8,18 @@ router = APIRouter()
 async def get_testroute(user: dict = Depends(get_user)):
     return user
 
+@router.get("/dashboard/lifecycle")
+async def get_lifecycle(user: dict = Depends(get_user)):
+    return stored_data.output("lifecycle")
+
 @router.get("/settings/metadata")
 async def get_metadata(user: dict = Depends(get_user)):
-    print(stored_data.output("metadata"))
     return stored_data.output("metadata")
 
-@router.get("/dashboard/lifecycle")
-async def get_metadata(user: dict = Depends(get_user)):
-    return user
+@router.post("/devices")
+async def get_devices(user: dict = Depends(get_user)):
+    return stored_data.output("queryresults_cves")
 
-@router.get("devices/views/saved")
-async def get_metadata(user: dict = Depends(get_user)):
-    return user
-
-@router.get("users/views/saved")
-async def get_metadata(user: dict = Depends(get_user)):
-    return user
+@router.get("/devices/views/saved")
+async def get_saved_queries(user: dict = Depends(get_user)):
+    return stored_data.output("savedqueries")
